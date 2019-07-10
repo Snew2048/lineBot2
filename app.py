f9749911ba1f4bd0bd90e41e9df9dc045e376f58
snew2048 <51729129+Snew2048@users.noreply.github.com>
2019-07-10 09:22:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (userText == 'สวัสดี') :
+        sendText(user,'สวัสดีจร้า ไอ้หน้าหี')
+    elif (userText == 'นี่ใครจ๊ะ') :
+        sendText(user,'ฉันประยุทธ์นะ')
+    else :
+        sendText(user,'มึงพิมพ์ไร กูไม่เข้าใจ ควายยยย')
+
     return '',200
 
 def sendText(user, text):
